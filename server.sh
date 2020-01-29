959c8cb0c726e6173771071ea18928c3b940c387
EC2 Default User <ec2-user@ip-172-31-21-7.us-east-2.compute.internal>
2020-01-29 07:22:49 +0000

    server.log

---
+++ server.sh
@@ -0,0 +1,11 @@
+while read line
+        do
+                host=`echo "$line" | cut -d " " -f1`
+                user=`echo $line | cut -d " " -f2`
+                pass=`echo "$line" | cut -d " " -f3`
+                sshpass -p "$pass" scp /home/ansadmin/test.yml $user@$host:/home/ansadmin
+
+        done < user.log
+> user.log
+
+
