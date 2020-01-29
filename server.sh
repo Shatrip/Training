bdac5ab85377e359e59fe744ebeb586a5c89e284
EC2 Default User <ec2-user@ip-172-31-21-7.us-east-2.compute.internal>
2020-01-29 09:50:20 +0000

    changed server.sh

--- server.sh
+++ server.sh
@@ -3,9 +3,7 @@ while read line
                 host=`echo "$line" | cut -d " " -f1`
                 user=`echo $line | cut -d " " -f2`
                 pass=`echo "$line" | cut -d " " -f3`
-                sshpass -p "$pass" scp /home/ansadmin/test.yml $user@$host:/home/ansadmin
+                sshpass -p "$pass" scp /home/ansadmin/test.yml "$user"@"$host":/home/ansadmin
 
-        done < user.log
-> user.log
-
+        done < /var/lib/jenkins/workspace/Test1/user.log
 
